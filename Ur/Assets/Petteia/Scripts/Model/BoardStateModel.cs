4ae8c6505658b68071a65571acf0d8216a3f4914
Kevin Dressel <kddressel@gmail.com>
2024-03-01 01:18:28 -0500

    working board state model and rules

---
+++ Ur/Assets/Petteia/Scripts/Model/BoardStateModel.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Petteia.Scripts.Model
+{
+    public class GameModel<T> where T : RulesSet
+    {
+        public RulesSet Rules;
+        public PlayerModel[] Players;
+        public BoardStateModel Board;
+
+        public GameModel(PlayerDef[] players, int boardWidth, int boardHeight)
+        {
+            var playerModels = new List<PlayerModel>();
+            foreach (var player in players)
+            {
+                var playerModel = new PlayerModel
+                {
+                    Def = player,
+                    Agent = (IPlayerAgent)Activator.CreateInstance(player.AgentType)
+                };
+                playerModels.Add(playerModel);
+            }
+            Players = playerModels.ToArray();
+
+            Board = new BoardStateModel(boardWidth, boardHeight);
+            Rules = new RulesSet();
+        }
+    }
+
+    #region Parameters for setting up a new game
+    public class PlayerDef
+    {
+        public string Name { get; set; }
+        public Type AgentType { get; set; }
+    }
+    #endregion
+
+    #region Agents
+    public interface IPlayerAgent
+    {
+
+    }
+
+    public class AIPlayerAgent : IPlayerAgent
+    {
+
+    }
+
+    public class HumanPlayerAgent : IPlayerAgent
+    {
+
+    }
+    #endregion
+
+    #region Needs categorization
+
+    public class PlayerModel
+    {
+        public PlayerDef Def { get; internal set; }
+        public IPlayerAgent Agent { get; internal set; }
+    }
+
+    /// <summary>
+    /// Most basic ruleset, to be extended and overridden with various custom rulesets
+    /// </summary>
+    public class RulesSet
+    {
+        public PlayerModel PlayerTurn { get; private set; }
+
+        virtual public void StartFirstTurn(PlayerModel[] players)
+        {
+            PlayerTurn = players.FirstOrDefault();
+        }
+
+        virtual public void AdvanceToNextTurn(PlayerModel[] players)
+        {
+            if (PlayerTurn == players[0]) PlayerTurn = players[1];
+            else if (PlayerTurn == players[1]) PlayerTurn = players[0];
+        }
+
+        virtual public IEnumerable<Vector2Int> GetSpacesToCaptureInMove(BoardStateModel board, Vector2Int from, Vector2Int to)
+        {
+            var isValid = IsValidMove(board, from, to);
+            var adjacentEnemySpaces = board.GetAdjacentSpaces(to).Where(space => !space.IsEmpty && space.Piece.Owner != PlayerTurn);
+            
+            // it is supposed to multi-capture if you surround pieces in multiple directions
+            // moving your piece between two enemy pieces isn't a capture. enemy has to move to capture, so we need to check who's turn it is
+            // TODO: Is it supposed to capture every piece in a line if there are multiple?
+            foreach(var enemySpace in adjacentEnemySpaces)
+            {
+                var capturingSpace = board.GetNextInSameDirection(to, enemySpace.Pos);
+                if(capturingSpace != null && capturingSpace.Piece.Owner == PlayerTurn)
+                {
+                    yield return enemySpace.Pos;
+                }
+            }
+
+            // TODO: Kennesaw students added a special rule for capturing in the 4 corners of the board, since those would be safe spaces otherwise
+        }
+
+        virtual public bool IsValidMove(BoardStateModel board, Vector2Int from, Vector2Int to)
+        {
+            var isStraightLine = board.IsStraightLine(from, to);
+            var isNotOccupied = board.IsSpaceEmpty(to);
+            var isNotBlocked = board.GetLineOfSpaces(from, to).All(space => space.IsEmpty);
+
+            return isStraightLine && isNotOccupied && isNotBlocked;
+        }
+    }
+
+    #endregion
+
+    #region Board
+    public class PieceModel
+    {
+        public PlayerModel Owner { get; private set; }
+
+        public PieceModel(PlayerModel owner)
+        {
+            Owner = owner;
+        }
+    }
+
+    public class SpaceModel
+    {
+        public Vector2Int Pos { get; private set; }
+        public PieceModel Piece { get; internal set; }
+        public bool IsEmpty => Piece == null;
+
+        public SpaceModel Clone()
+        {
+            return new SpaceModel(Pos) { Piece = Piece };
+        }
+
+        public SpaceModel(Vector2Int pos)
+        {
+            Pos = pos;
+        }
+    }
+
+    public class BoardStateModel
+    {
+        // TODO: Consider representing this as lists of pieces instead for performance in AI move space searching
+        readonly SpaceModel[,] _spaces;
+
+        public Vector2Int Size => new Vector2Int(_spaces.GetLength(0), _spaces.GetLength(1));
+
+        public BoardStateModel(int width, int height)
+        {
+            _spaces = new SpaceModel[width, height];
+            for (var x = 0; x < Size.x; x++)
+            {
+                for (var y = 0; y < Size.y; y++)
+                {
+                    _spaces[x, y] = new SpaceModel(new Vector2Int(x, y)) { Piece = null };
+                }
+            }
+        }
+
+        public BoardStateModel Clone()
+        {
+            var clone = new BoardStateModel(Size.x, Size.y);
+            for (var x = 0; x < Size.x; x++)
+            {
+                for (var y = 0; y < Size.y; y++)
+                {
+                    clone._spaces[x, y] = _spaces[x, y].Clone();
+                }
+            }
+            return clone;
+        }
+
+        public BoardStateModel PlaceNewPiece(PlayerModel forPlayer, SpaceModel onSpace)
+        {
+            Debug.Assert(onSpace.Piece == null);
+
+            var clone = Clone();
+            clone.GetSpaceAt(onSpace.Pos).Piece = new PieceModel(forPlayer);
+            return clone;
+        }
+
+        public BoardStateModel RemovePiece(SpaceModel onSpace)
+        {
+            var clone = Clone();
+            clone.GetSpaceAt(onSpace.Pos).Piece = null;
+            return clone;
+        }
+
+        public BoardStateModel MovePiece(SpaceModel fromSpace, SpaceModel toSpace)
+        {
+            var clone = Clone();
+            var movePiece = fromSpace.Piece;
+            clone.GetSpaceAt(fromSpace.Pos).Piece = null;
+
+            Debug.Assert(movePiece != null);
+
+            clone.GetSpaceAt(toSpace.Pos).Piece = movePiece;
+
+            return clone;
+        }
+
+        public SpaceModel GetSpaceAt(Vector2Int pos)
+        {
+            if (pos.x < 0 || pos.y < 0 || pos.x > Size.x || pos.y > Size.y) return null;
+            else return _spaces[pos.x, pos.y];
+        }
+
+        public IEnumerable<SpaceModel> GetColumnOfSpaces(int xColumn)
+        {
+            for (var y = 0; y < Size.y; y++)
+            {
+                yield return _spaces[xColumn, y];
+            }
+        }
+
+        public IEnumerable<SpaceModel> GetRowOfSpaces(int yRow)
+        {
+            for (var x = 0; x < Size.x; x++)
+            {
+                yield return _spaces[x, yRow];
+            }
+        }
+
+        public bool IsStraightLine(Vector2Int from, Vector2Int to) => IsVerticalStraightLine(from, to) ^ IsHorizontalStraightLine(from, to);
+        public bool IsVerticalStraightLine(Vector2Int from, Vector2Int to) => from.x == to.x;
+        public bool IsHorizontalStraightLine(Vector2Int from, Vector2Int to) => from.y == to.y;
+
+        public IEnumerable<SpaceModel> GetLineOfSpaces(Vector2Int from, Vector2Int to)
+        {
+            if (IsHorizontalStraightLine(from, to))
+            {
+                var row = GetRowOfSpaces(from.y);
+                if(from.x < to.x)
+                {
+                    // left to right
+                    return row.SkipWhile(space => space.Pos.x <= from.x).TakeUntil(space => space.Pos.x == to.x);
+                }
+                else
+                {
+                    // right to left
+                    return row.Reverse().SkipWhile(space => space.Pos.x <= from.x).TakeUntil(space => space.Pos.x == to.x);
+                }
+            }
+            else if (IsVerticalStraightLine(from, to))
+            {
+                var col = GetColumnOfSpaces(from.x);
+                if (from.y < to.y)
+                {
+                    // bottom to top
+                    return col.SkipWhile(space => space.Pos.y <= from.y).TakeUntil(space => space.Pos.y == to.y);
+                }
+                else
+                {
+                    // top to bottom
+                    return col.Reverse().SkipWhile(space => space.Pos.y <= from.y).TakeUntil(space => space.Pos.y == to.y);
+                }
+            }
+            else
+            {
+                return Enumerable.Empty<SpaceModel>();
+            }
+        }
+
+        public IEnumerable<SpaceModel> GetAdjacentSpaces(Vector2Int pos)
+        {
+            var left = GetSpaceAt(pos - new Vector2Int(1, 0));
+            var right = GetSpaceAt(pos + new Vector2Int(1, 0));
+            var below = GetSpaceAt(pos - new Vector2Int(0, 1));
+            var above = GetSpaceAt(pos + new Vector2Int(0, 1));
+
+            if (left != null) yield return left;
+            if (right != null) yield return right;
+            if (below != null) yield return below;
+            if (above != null) yield return above;
+        }
+
+        public Vector2Int GetDir(Vector2Int from, Vector2Int to)
+        {
+            var diff = to - from;
+            var dir = new Vector2Int
+            (
+                diff.x != 0 ? Math.Sign(diff.x) : 0,
+                diff.y != 0 ? Math.Sign(diff.y) : 0
+            );
+
+            return dir;
+        }
+
+        public SpaceModel GetNextInSameDirection(Vector2Int from, Vector2Int to)
+        {
+            var dir = GetDir(from, to);
+            return GetSpaceAt(to + dir);
+        }
+
+        public PieceModel GetPieceAt(Vector2Int pos)
+        {
+            var space = GetSpaceAt(pos);
+            return space?.Piece;
+        }
+
+        public bool IsSpaceEmpty(Vector2Int pos)
+        {
+            var space = GetSpaceAt(pos);
+            return space != null ? space.IsEmpty : true;
+        }
+    }
+    #endregion
+}
---
+++ Ur/Assets/Petteia/Scripts/Model/GameModelTester.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Assets.Petteia.Scripts.Model
+{
+    public class GameModelTester : MonoBehaviour
+    {
+        GameModel<RulesSet> GameModel;
+
+        private void Awake()
+        {
+            var playerDefs = new PlayerDef[]
+            {
+                new PlayerDef { Name = "You", AgentType = typeof(HumanPlayerAgent) },
+                new PlayerDef { Name = "Other", AgentType = typeof(HumanPlayerAgent) }
+            };
+
+            GameModel = new GameModel<RulesSet>(playerDefs, 8, 8);
+        }
+
+#if UNITY_EDITOR
+        [CustomEditor(typeof(GameModelTester))]
+        public class LookAtPointEditor : Editor
+        {
+            string _player;
+            string _toXPos;
+            string _toYPos;
+            string _fromXPos;
+            string _fromYPos;
+
+            private void OnEnable()
+            {
+                var tester = target as GameModelTester;
+                var gameModel = tester.GameModel;
+
+                // everything underneath here requires play mode
+                if (!Application.isPlaying)
+                {
+                    return;
+                }
+
+                var rules = gameModel.Rules;
+                rules.StartFirstTurn(gameModel.Players);
+            }
+
+            public override void OnInspectorGUI()
+            {
+                var tester = target as GameModelTester;
+                var gameModel = tester.GameModel;
+
+                // everything underneath here requires play mode
+                if (!Application.isPlaying)
+                {
+                    EditorGUILayout.LabelField("Enter playmode to see more controls.");
+                    return;
+                }
+
+                var rules = gameModel.Rules;
+
+                EditorGUILayout.LabelField("Turn: " + gameModel.Rules.PlayerTurn?.Def.Name);
+
+                EditorGUILayout.LabelField("----Place Pieces----");
+                _player = EditorGUILayout.TextField("Player", _player);
+                _toXPos = EditorGUILayout.TextField("XPos", _toXPos);
+                _toYPos = EditorGUILayout.TextField("YPos", _toYPos);
+                if(GUILayout.Button("Place new Piece"))
+                {
+                    var player = gameModel.Players[int.Parse(_player)];
+                    var pos = new Vector2Int(int.Parse(_toXPos), int.Parse(_toYPos));
+
+                    gameModel.Board = gameModel.Board.PlaceNewPiece(player, gameModel.Board.GetSpaceAt(pos));
+                }
+
+                EditorGUILayout.LabelField("----Move Pieces----");
+                _fromXPos = EditorGUILayout.TextField("From XPos", _fromXPos);
+                _fromYPos = EditorGUILayout.TextField("From YPos", _fromYPos);
+                _toXPos = EditorGUILayout.TextField("To XPos", _toXPos);
+                _toYPos = EditorGUILayout.TextField("To YPos", _toYPos);
+                if(GUILayout.Button("Check Valid Move"))
+                {
+                    var fromPos = new Vector2Int(int.Parse(_fromXPos), int.Parse(_fromYPos));
+                    var toPos = new Vector2Int(int.Parse(_toXPos), int.Parse(_toYPos));
+
+                    Debug.Log("IS VALID MOVE: " + rules.IsValidMove(gameModel.Board, fromPos, toPos));
+                }
+                if (GUILayout.Button("Move Piece"))
+                {
+                    var fromPos = new Vector2Int(int.Parse(_fromXPos), int.Parse(_fromYPos));
+                    var toPos = new Vector2Int(int.Parse(_toXPos), int.Parse(_toYPos));
+
+                    gameModel.Board = gameModel.Board.MovePiece(gameModel.Board.GetSpaceAt(fromPos), gameModel.Board.GetSpaceAt(toPos));
+
+                    // do captures
+                    var captures = gameModel.Rules.GetSpacesToCaptureInMove(gameModel.Board, fromPos, toPos);
+                    foreach(var capture in captures)
+                    {
+                        gameModel.Board = gameModel.Board.RemovePiece(gameModel.Board.GetSpaceAt(capture));
+                    }
+
+                    // usually you want to be able to move the piece again, so swap them each move
+                    _fromXPos = _toXPos;
+                    _fromYPos = _toYPos;
+                }
+
+                if(GUILayout.Button("End Turn"))
+                {
+                    gameModel.Rules.AdvanceToNextTurn(gameModel.Players);
+                }
+
+                // draw the board
+                for (var y = 0; y < gameModel.Board.Size.y; y++)
+                {
+                    var row = "";
+                    for(var x = 0; x < gameModel.Board.Size.x; x++)
+                    {
+                        var space = gameModel.Board.GetSpaceAt(new Vector2Int(x, y));
+                        if (space.IsEmpty)
+                        {
+                            row += " _ ";
+                        }
+                        else if(space.Piece.Owner == gameModel.Players[0])
+                        {
+                            row += " X ";
+                        }
+                        else if(space.Piece.Owner == gameModel.Players[1])
+                        {
+                            row += " O ";
+                        }
+                        else
+                        {
+                            row += "err";
+                        }
+                    }
+                    EditorGUILayout.LabelField(row);
+                }
+            }
+        }
+#endif
+    }
+}
